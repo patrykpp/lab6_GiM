be71dc66da82d43097b44d39167471787ba959f9
patrykpp <patryk583@vp.pl>
2018-04-16 20:19:19 +0200

    lab6_GiM

---
+++ Assets/Scripts/AnimationControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+
+    // Use this for initialization
+    Animator scriptToControl;
+
+    void Start()
+    {
+        scriptToControl = gameObject.GetComponent<Animator>();
+		scriptToControl.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            scriptToControl.enabled = !scriptToControl.enabled;
+        }
+
+    }
+}
---
+++ Assets/Scripts/AnimationControl2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    Animator scriptToControl;
+
+    void Start()
+    {
+        scriptToControl = gameObject.GetComponent<Animator>();
+		scriptToControl.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            scriptToControl.enabled = !scriptToControl.enabled;
+        }
+
+    }
+}
